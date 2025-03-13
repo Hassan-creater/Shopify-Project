a7dd771405aa5504d1e5eef67a34014a335563ef
Hassan-creater <034217602ab@gmail.com>
2025-03-13 01:29:51 -0700

    First Push

---
+++ assets/application.js
@@ -0,0 +1,43 @@
+const menu = document.querySelector(".menu");
+const cross = document.querySelector(".cross");
+const menuList = document.querySelector(".menuList");
+
+
+// const show = ()=>{
+//      menu.addEventListener("click",()=>{
+//             menuList.classList.remove("hidden");
+//             menuList.classList.add("block");
+//      },{once:true})
+// }
+
+
+
+// const hide = ()=>{
+//     menu.addEventListener("click",()=>{
+//            menuList.classList.add("hidden");
+//            menuList.classList.remove("block");
+//     },{once:true})
+// }
+
+// if(menuList.classList.contains("hidden")){
+//     menu.addEventListener("click",()=>{
+//         menuList.classList.remove("hidden");
+//         menuList.classList.add("block");
+//  },{once:true})
+// }
+// if(menuList.classList.contains("block")){
+//     menu.addEventListener("click",()=>{
+//         menuList.classList.add("hidden");
+//         menuList.classList.remove("block");
+//  },{once:true})
+// }
+
+
+// menu.addEventListener('click',()=>{
+//     menu.classList.add('hidden');
+    
+// })
+
+// cross.addEventListener("click",()=>{
+//     console.log("helo");
+// })
